c9c1e13cd14988e6a533baf2534730f85fb1a032
bhakti hansalpara <bhakti.hansalpara@saeculum.com>
2023-06-25 18:48:31 +0530

    23 june

--- 20 june/operators_ex.js
+++ 20 june/operators_ex.js
@@ -17,28 +17,29 @@ var x=3;
 var y=2;
 var z=4;
 var ans=x*(y+z);
-console.log(ans);  //ans=18
+console.log(ans);  //ans=18 here breaket is first consider after other oprestion perfom
 
 4
 var x=8;
 var y=2;
 var z=2;
 var ans=x/(y+z);
-console.log(ans); //ans=2
+console.log(ans); //ans=2 breakets ara first solve then other operation perform
 
 5
 var x=15;
 var y=4;
 var z=2;
 var ans=x%y+z;
-console.log(ans); //ans =5
+console.log(ans); //ans =5  here % is higher presedance than it move to left to right 
+
 
 6
 var x=20;
 var y=4;
 var z=2;
 var ans=x/y-z;
-console.log(ans); //ans=3
+console.log(ans); //ans=3  here / is higher presedance than it move to left to right 
 
 7
 var x=2;
@@ -46,7 +47,8 @@ var y=3;
 var z=4;
 var b=2;
 var ans= x+ y* z / b;
-console.log(ans);  //ans =8
+console.log(ans);  //ans =8  when all value predence is same than see 
+                            //associativity here move to right to left first / then multiplication than addistion 
 
 8
 var x=4;
@@ -60,21 +62,24 @@ var x=10;
 var y=2;
 var z=3;
 var ans=(x+y)/z;
-console.log(ans);  //ans=4
+console.log(ans);  //ans=4  first slove breakets
 
 10
 var x=6;
 var y=2;
 var z=4;
 var ans=x*y%z;
-console.log(ans); //ans=0  
+console.log(ans); //ans=0  //precedence of multiplication is greater than remainder, so multiplication will be 
+                           //first then remainder
+          
+
 
 11
 var x=2;
 var y=3;
 var z=2;
 var ans=x**y**z;
-console.log(ans); //ans =512  64**2=512
+console.log(ans); //ans =512  2**9=512  
 
 
 12
@@ -82,7 +87,8 @@ var x=4;
 var y=2;
 var z=3;
 var ans=(x+y)**3;
-console.log(ans); //ans=216  6*3=216
+console.log(ans); //ans=216  6*3= 216 //precedence of parenthesis is greater than exponentiations, so addition inside 
+                                      //parenthesis will be first then exponentiations
 
 13
 var x=2;
@@ -95,80 +101,307 @@ console.log(ans)  //ans=50 2+3*16  48+2  50
 console.log(2 + 3 * 4 ** 2);
 
 14
-console.log( 6 * (2 + 4) / 3); //ans=12
+console.log( 6 * (2 + 4) / 3); //ans=12  here first slove breakts after *,/ presedance is same so it
+                                        //  move to left to right to alove that
 
 15
-console.log( (10 - 2) / 4);// ans=2
+console.log( (10 - 2) / 4);// ans=2 //first slove breakts in addistion than dividtion
 
 16
-console.log(8 / (2 * 2)); //ans=2
+console.log(8 / (2 * 2)); //ans=2 //here fisrt slove brackts multiplication after divistion is apply
 
 17
-console.log( 15 % (4 + 2)); //ans-3
+console.log( 15 % (4 + 2)); //ans-3 here brackets are first slove than % is apply
 
 18
-console.log( 20 / (4 - 2) );//ans=10
+console.log( 20 / (4 - 2) );//ans=10 here brackts are first slove than apply /
 
 19
-console.log( 2 + (3 * 4) / 2);//ans=8
+console.log( 2 + (3 * 4) / 2);//ans=8 here brackets are first slove than division is apllly beacuse 
+                                   //accoding to associtiy first alove / right to left than apply addision
 
 20
-console.log(4 / (2 ** 2));//ans=1
+console.log(4 / (2 ** 2));//ans=1 //first slove breackts after divistion
 
 21
-console.log((10 + 2) / 3);//ans=4
+console.log((10 + 2) / 3);//ans=4 first slove brackets after divistion
 
 22
-console.log(6 * (2 % 4));//ans=12
+console.log(6 * (2 % 4));//ans=12 //first slove breackets after mulipication
 
 23
-console.log(2 ** (3 ** 2));//ans=512
+console.log(2 ** (3 ** 2));//ans=512 //first slove breackerts after power
 
 24
-console.log((4 + 2) ** 3);//ans=216
+console.log((4 + 2) ** 3);//ans=216  //first slove bfreackts after power
 
 25
-console.log( 2 + (3 * 4 ** 2));// right to left//ans=50
+console.log( 2 + (3 * 4 ** 2));// right to left//ans=50 fist slove breackts in breackes ** is grater
+                              // associtity first ** than * after addistion is apply
 
 26
-console.log( 6 * (2 + 4) % 3);//ans=0
+console.log( 6 * (2 + 4) % 3);//ans=0 frist slove breackets *,/ associvity is same to move left to right
 
 27
-console.log((10 - 2) / 4); //ans=2
+console.log((10 - 2) / 4); //ans=2 here fisrt slove breackets after divistion
 
 28
-console.log(8 / (2 + 2));//ans=2
+console.log(8 / (2 + 2));//ans=2 here first slove breackts addition than divistion is apply
 
 29
-console.log(15 % (4 - 2));//ans=1
+console.log(15 % (4 - 2));//ans=1 here fisrt slove breackts sub(-) after % is apply
 
 30
-console.log( 20 / 4 - 2);//ans=3
+console.log( 20 / 4 - 2);//ans=3 here first slove divistion bcz / is higher associvity than -
 
 31
-console.log( 2 + 3 * (4 / 2));//r to l ans=8
+console.log( 2 + 3 * (4 / 2));//r to l ans=8 here first slove brackets after * bcz in +and * 
+                            //associvity is same than move to right to left .
 
 32
-console.log(4 / (2 ** 2));//ans1
+console.log(4 / (2 ** 2));//ans1 here breacks are slove first than division is apply
 
 33
-console.log((10 + 2) / 3);//ans-4
+console.log((10 + 2) / 3);//ans-4 here breacks are first apply than divistion is apply
 
 34
-console.log( 6 * (2 % 4) - 3);//ans=9
+console.log( 6 * (2 % 4) - 3);//ans=9 here breacks are first slove so slove in breacks % after * than -
 
 35
-console.log(2 ** (3 ** 2));//ans-512
+console.log(2 ** (3 ** 2));//ans-512 here first slove bracks  after slove power** 
 
 36
-console.log((4 + 2) ** 3 - 1);//215
+console.log((4 + 2) ** 3 - 1);//215 here first slove bracks additon after ** and than -
 
 37
-console.log(2 + (3 * 4 ** 2) / 2);//26
+console.log(2 + (3 * 4 ** 2) / 2);//26 here fisrt slove bracks in bracks first slove ** and multiplication 
+                                  //than diviston than addition 
 
 38
-console.log(6 * (2 + 4) % 3 + 1);//ans1
+console.log(6 * (2 + 4) % 3 + 1);//ans1 here slove brackets 1st after  * after % after + left to right
 
 39
-console.log((10 - 2) / 4 + 2);//4
+console.log((10 - 2) / 4 + 2);//4 here fist slove bracktes after divition than +
 
+40
+console.log(8 / (2 * 2) + 3);//ans-5 first slove breckts after divition than +
+
+41
+console.log(15 % (4 + 2) - 1);//ans-2 first slove brackets after % after -
+
+42
+console.log(20 / (4 - 2) + 1 );//ans11 first slove brackets agfter diviton after +
+
+43
+console.log( 2 + (3 * 4) / 2 - 1);//ans=7 first slove brackets after divition after plus
+
+44
+console.log(4 / (2 ** 2) + 3);//ans=4 first slove breackets after divition after +
+
+45
+console.log(4 / (2 ** 2) + 3);//ans=4 first slove brackts after divition than plus
+
+46
+console.log( 6 * (2 % 4) / 3 + 2 );//ans=6 first slove brackets after multiply after diviton after +
+
+47
+console.log(2 ** (3 ** 2) - 1);//ans=511 fisrt slove breackts after remainder after -
+
+48
+console.log( (4 + 2) ** 3 + 1);//217 first slove brackts after reminder after +
+
+49
+console.log( 2 + (3 * 4 ** 2) / 2 - 3);//ans=23 first slove brackets expolainsir than multiply after divition
+
+
+50
+console.log(6 * (2 + 4) % 3 + 1 - 2);//ans=-1 frist slove beackets after multiplaction after % after +after_
+
+51
+console.log( "5" + "3");//53   //both are string than concat two string ->53
+
+52
+console.log("7" - "2");//5  //minus(-) is used to substract two value
+
+53
+console.log( "10" * "2");//20 //multiplication(*) is used to multiply two number or string
+
+54
+console.log("15" / "5");//3//divison(/) is used to divide two number or string
+
+55
+console.log( "10" % "3" );//1 //Module(%) is used to return reminder string
+
+56
+console.log(("2.5") + 1.8);//2.51. //plus operator is used to concat two string if value are string
+
+57
+console.log( 4 - Number("2.5"));//ans=1.//Number is used to change string into number
+
+58
+console.log(1.5 * Number("3.2"));//ans=4.800000000000001 //string into number-multiply
+
+59
+console.log( 9.8 / Number("2"));//4.9   //string into number-divide
+
+60
+console.log( "7" + String(2.5));//ans=72.5 it in string + is unary,bainary.
+
+61
+console.log(10 - Number("2.5"));//7.5
+
+62
+console.log(3.5 * Number("2"));//ans=7
+
+63
+console.log(8 / Number("2.5")); //string into number-divide
+
+64
+console.log( String(1.2) + "0.5");//concat
+// console.log( String(1.2) - "0.5");
+
+65
+console.log(6 - Number("1.2"));  //string into number-minus
+
+66
+console.log(10 * 2 / 5 + Math.pow(2, 3) - 4 % 3);//math-left to right(multiply-divide)-module-plus-minus
+
+67
+console.log( "hello" && 0);//ans=0  //first condition true than print second statement in &&
+
+68
+console.log( "" || "world");//ans=world /first condition false than print second statement in ||
+
+69
+console.log(null && true);//ans=null /first condition false than print 1st statement in ||
+
+70
+console.log(NaN || "value");//ans=value  ///first condition false than print second statement in ||
+
+71
+console.log(undefined && null);//ans=undefined ///first condition false than print first statement in &&
+
+72
+console.log(10 && "number");//number first condition true than print second statement in &&
+
+73
+console.log( " " || 0);//ans=0 //first condition false than print second statement in ||
+
+74
+console.log( NaN && "value");//NAN first condition false than print first statement in &&
+
+75
+console.log( null || undefined); //first condition false than print second statement in ||
+
+76
+console.log( false && "false");//false ///first condition false than print second statement in ||
+
+77
+console.log( "hello" || 0); ///first condition false than print second statement in ||
+
+78
+console.log( 0 && true);//ans=0///first condition false than print second statement in ||
+
+79
+console.log( NaN || null);//ans=null // //first condition false than print second statement in ||
+
+
+80
+console.log( undefined && "undefined" );//undefined //first condition false than print first statement in &&
+
+81
+console.log(10 || "number");//10 ");        
+//first condition true than print first statement in ||
+
+
+82
+console.log(  "" && 0);//space print  //first condition false than print first statement in &&
+
+83
+console.log( NaN || "");//space //first condition false than print second statement in ||
+
+
+84
+console.log( null && undefined);//null //first condition false than print first statement in &&
+
+85
+console.log( false || "false");//string-false //first condition false than print second statement in ||
+
+
+86
+console.log( "hello" && 10);//10//first condition true than print second statement in &&
+
+87
+console.log( true ? "Yes" : "No");//yes/condition is true than print first statement
+
+88
+console.log( false ? 10 : 5);//5 //condition is false than print second statement
+
+89
+console.log(3 > 5 ? "Greater" : "Less");//less   //condition is false than print second statement
+
+90
+console.log( 10 === 10 ? "Equal" : "Not equal");//equal //condition is true than print first statement
+
+91
+console.log( false ? "Hello" : "World");//world   //condition is false than print second statement
+
+92
+console.log(null ? "NotNull" : "Null");//null  //condition is false than print second statement
+
+93
+console.log( undefined ? "Defined" : "Undefined");//undefined//condition is false than print second statement
+
+94
+console.log( 'a' !== 'b' ? "Different" : "Same");//diffrent //condition is true than print first statement
+
+95
+console.log(1 < 0 ? "True" : "False");//false//condition is false than print second statement
+
+96
+console.log( 0 ? "Truthy" : "Falsy");//falsy//condition is false than print second statement
+
+97
+console.log( "" ? "Truthy" : "Falsy");//falsy bcz it empty //condition is false than print second statement
+
+98
+console.log( NaN ? "Valid" : "Invalid");//invaild  //condition is false than print second statement
+
+99
+console.log( [] ? "NotEmpty" : "Empty");//not empty //condistion is true than print first statemnet
+
+100
+console.log( [1, 2] ? "Exists" : "Not exists");//Exists//condition is true than print first statement
+
+101
+console.log( {} ? "Object" : "Not object");//object//first condistion is ture than print first statement
+
+102
+console.log( 0.5? "True" : "False");//ture buz we have value when we write 0 than give false
+
+103
+console.log(undefined === null ? "Equal" : "Not equal");//not-equale first conditon is ture than print second statment
+
+104
+console.log( 10 > 5 ? "Greater" : "Less");//greater//condition is true than print first statement
+
+105
+console.log(null !== undefined ? "Different" : "Same");//it is ture than print diffreent;//condition is true than print first statement
+
+106
+console.log( true ? 1 : 0);//1 //condition is true than print first statement
+
+107
+var num1=9;
+var num2=6;
+var num3=5;
+
+largest= (num1>num2)?(num1>num3?num1:num3):(num2>num3?num2:num3);
+largest=(num1>num2)?(num1>num3):a(num2>num1)
+// largest=(a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+console.log(largest); 
+//here fist cheack which number is bigger than two numbers than first cheack 1,2 num if 1st number is big than 
+//2nd number so 1st is also cheack wich 3rd number than both condition ture than print 1st number
+
+//same which 2nd number is great than 1st and 3rd than print it both condision is ture than print 2nd satatement
+//if both acondidtion are false than 3rd is print.
---
+++ 22 june/string.js
@@ -0,0 +1,567 @@
+let a = "java";
+let b = a.concat('script');
+console.log(b);   // javascript
+
+console.log("welcome");
+console.log('Bhakti');
+console.log(`days`);
+
+//string store in virable using primitive// save is like string
+var str='hello';
+console.log(str);
+console.log(typeof(str));
+console.log(str,(typeof str));
+
+let myvar;
+myvar=String('bhakti');
+console.log(myvar,(typeof myvar));
+
+let boolenVar='ture';
+console.log(boolenVar,(typeof boolenVar));
+let boolenVar=String(ture);
+console.log(boolenVar,(typeof boolenVar));
+
+let date=new Date();
+console.log(date,(typeof Date));
+let date=String(new Date());
+console.log(date,(typeof Date));
+
+let arr=String([1,2,3,4]);
+console.log(arr,(typeof arr));
+
+
+//string store in variable using constructor // save is like object
+var str=new String('hello');
+console.log(str);
+console.log(typeof(str));
+console.log(typeof str);
+console.log(str, (typeof str));
+
+//in one sentence not allow doubtime doubcodes or singlecods//
+console.log("hello everione i m 'bhakti'");
+console.log('hello I am "bhakti"from rajkot');
+console.log(`helo 'good morning'`);
+console.log("godd one that `is`");
+console.log(`hello
+      gyes what about you
+      what are you doing`);
+      console.log('helo');
+
+      //lenth -proprty also count space
+    var str="bhakti";
+      console.log(str.length);
+      var str='Dhruv and Dhara';
+      console.log(str.length);
+      console.log(str[4]);
+      console.log(str[20]);
+      console.log(str[-1]);
+      console.log(str[str.length-1]);
+      let arr=String([1,2,3,4,5]);   //string count char //here ans=9
+      console.log(arr.length,(typeof arr));
+      let arr=[1,2,3,4,5];                     //arry count element//here ans=5
+      console.log(arr.length,(typeof arr));
+     
+// lengh 2nd methode to call is tostring
+      let i=8;
+      console.log(i.toString());
+
+    //   counvrting value of string
+    var number=7;
+    number=number.toString();
+    console.log(typeof number);//in this methde a problem in null and undifend
+    var num;
+    var boolen=true;
+    var type=null;
+    var number=9;
+    console.log(String(num),typeof num);
+    console.log(String(boolen));
+    console.log(String(type,typeof null));
+    console.log(String(number));
+
+
+//compairing string
+
+var str1="bhakti";
+var str2="bhakti";
+console.log(str1.compareTo(str2));
+
+var str1="bhakti";
+var str2="bhakti";
+console.log(str1.localeCompare(str2));//it gives 0 bcz both are same
+
+var str1="bhkti";
+var str2='KRISTI';
+console.log(str1.localeCompare(str2));//it gives -1 bcz value of k unicode is less than k. 
+
+
+var str1="bhkti";
+var str2='amisha';
+console.log(str1.localeCompare(str2));//it gives 1 bcz value of a unicode is less than b
+
+//-----charAt-----method------//
+//it returns at a specified index(position) in string.
+var str1="hello everyone";
+console.log(str1.charAt(4));
+console.log(str1.charAt());
+
+//------AT-----methd ------//
+//it use in arry to specifeid index(position) in array.
+var arr=[1,22,4,35,5];
+console.log(arr.at(2));
+
+//-----charcodeAT----methd----//
+//it return of specified index numb er unicode.
+var name="bhakti";
+console.log(name.charCodeAt(9));
+console.log(name.charCodeAt(1));
+
+//---stratwith---methd----//
+//it show that we define a word in prifix is that if yes than return ture ot than return false
+var name="bhakti patel";
+console.log("reuslut: "+name.startsWith("bha"));
+var name="bhakti";
+console.log("reuslut: "+name.startsWith("bha"));
+
+//
+//---stratwith---methd----//
+//it show that we define a word in suffix is that if yes than return ture ot than return false
+//it gives boolen value only
+
+var name="bhakti";
+console.log("reuslut: "+name.endsWith("ti"));
+
+//includes ----it check that this element or this char or that number is in this string or not
+//it gives also only boolean value ture and false
+var a="bhakti!";
+var b=a.includes("bh");
+console.log(b);
+
+var a="hello gyus! I am Bhakti."
+console.log(a.includes("I",13));
+console.log(a.includes("gyus"));
+console.log(a.length);
+console.log(a.includes("h",1));
+
+//--indexOf---//
+//it check that the char is which index no id belong which you search//
+//when it gives -1 than that means this is not found in this statement.
+var str="java-script is complex language";
+console.log(str.indexOf("t"));
+console.log(str.indexOf("-",10));
+console.log(str.indexOf("e",22));
+console.log(str.indexOf("is"));
+
+//lastIndexOf--//
+//it cheack to last find index no .
+var str="good Mornig-one Every-one";
+console.log(str.lastIndexOf("one"));
+console.log(str.indexOf("one"));
+
+//--match---//
+//in when the value not found than it gives null.
+var a="9887kjb ssdkn389uhbjb";
+console.log(a.match(/87/));
+console.log(a.match("ssd"));
+console.log(a.match("9887kjb"));
+console.log(a.match("bhakti"));
+console.log(a.matchAll("ssd"));
+
+//---matchAll----//
+var a="I am Bhakti Patel";
+console.log(a.matchAll("Bhakti"));
+
+//---padstart---//
+//syntax that padstart after index no
+//total index-number is space than our elelment is inclued.
+"abc".padStart(10);
+var a="bhakti";
+console.log(a.padStart(10));
+console.log(a.padStart(20,"123456789"));
+
+//---padEnd----//
+//in this space is not allowed //
+var a="good evening";
+console.log(a.padEnd(10));
+console.log(a.padEnd(10,"bhakti"));
+console.log("abc".padEnd(10,'bha'));
+console.log("good".padEnd(10,'bhs'));
+
+//---replace---//
+//it is use to replace elememt or word.
+//sntax is that .respace("replace word",("write the word which you replace"))
+var a="heloo every-one and sir,mam";
+console.log(a.replace("and","&"));//here and is replace by &
+console.log(a.replace(" ","&"));  //WHEN YOU NOT DEFINED WHICH ELELMT YOU RESPCE THAN IT CHOOCE SPACE.
+console.log(A.replace(/d/g,"22"));
+
+//----search----//
+
+//it not count space
+let a = 'i am bhakti';
+let b = a.search(/am/g);
+console.log(b);        
+
+
+
+
+
+
+//----split-----//
+//it romeve the elemet which you split
+var a="b hakti how are you";
+// console.log(a.split(" "));
+// console.log(a.split("k"));
+console.log(a.split("kti"));
+
+let a = "cow is a animal"
+let b = a.split('s');
+console.log(b); 
+
+
+//----slice-----//
+//frist is that to start and 2nd is when end 
+var a="bhakti what are you doing";
+console.log(a.slice(1,4));//ans=hak
+console.log(a.slice(-1));//g
+console.log(a.substring(1,4));//ans=hak
+console.log(a.slice(0));
+console.log(a.slice(-15));
+
+//----subSrting-------//
+//in subString 1st strat from and 2nd is stop but 2nd is not print.
+var str="bhakti how are you"
+console.log(str.substring(2,6));
+
+//----toUpperCase-----//
+var a="bhakti when you come?"
+console.log(a.toUpperCase());
+
+//----toLowerCase-----//
+var a="where are you going?"
+console.log(a.toLowerCase(0,1));
+
+//----trim-----//
+var a="    bhakti    ";
+console.log(a.trim());
+
+//---trim-start-----//
+var a="     whrer are you     ";
+console.log(a.trimStart());
+
+//----trim-end------//
+var a="    whre are you  ";
+console.log(a.trimEnd());
+console.log("contect :"+a.trimEnd()+" ");
+console.log("contect :"+a.trimLeft()+" ");
+console.log("contect :"+a.trimRight()+" ");
+var alphabats="\t\n  A  B\n  C  D...\n";
+
+console.log(alphabats.trimStart());
+console.log(alphabats);
+
+
+var a="Bhakti";
+console.log(a.length);
+
+12
+
+const str = "HelloWorld";
+console.log(str.toLowerCase());
+
+
+13
+helloworld
+const sentence = "I love JavaScript!";
+console.log(sentence.indexOf("JavaScript"));
+
+14
+
+const name = "John";
+const greeting = `Hello, ${name}!`;
+console.log(greeting);
+
+15
+const text = "Hello, World!";
+console.log(text.substring(7));
+
+16
+const message = "Hello, World!";
+console.log(message.replace("World", "Universe"));
+
+17
+const words = "apple, banana, orange";
+const fruits = words.split(", ");
+console.log(fruits);
+
+18
+
+const text = "   Hello, World!   ";
+console.log(text.trim());
+
+19
+const sentence = "The quick brown fox jumps over the lazy dog.";
+console.log(sentence.includes("dog"));
+
+20
+const str = "Hello, World!";
+console.log(str.endsWith("!"));
+
+
+21
+const sentence = "JavaScript is fun!";
+console.log(sentence.charAt(4));
+
+
+22
+const str1 = "Hello";
+const str2 = "World";
+
+
+23
+
+const sentence = "JavaScript is awesome!";
+console.log(sentence.slice(0, 10));
+
+24
+const text = "Hello, World!";
+console.log(text.repeat(3));
+
+25
+const sentence = "The quick brown fox";
+console.log(sentence.split(" "));
+
+26
+const str = "Hello, World!";
+console.log(str.indexOf("o"));
+
+27
+const sentence = "JavaScript is fun!";
+console.log(sentence.substring(4, 10));
+
+28
+const text = "Hello, World!";
+console.log(text.charAt(7));
+
+29
+const sentence = "I love JavaScript!";
+console.log(sentence.split("o"));
+
+30
+const str = "Hello, World!";
+console.log(str.includes("World"));
+
+31
+const sentence = "JavaScript is cool!";
+console.log(sentence.lastIndexOf("o"));
+
+32
+const text = "Hello, World!";
+console.log(text.startsWith("Hello"));
+
+33
+const str = "Hello, World!";
+console.log(str.toUpperCase());
+
+34
+const sentence = "JavaScript is awesome!";
+console.log(sentence.toLowerCase());
+
+35
+ const str = "Hello, World!";
+console.log(str.charAt(13));
+
+36
+const sentence = "JavaScript is fun!";
+console.log(sentence.substr(4, 10));
+
+37
+ const text = "Hello, World!";
+console.log(text.includes("WORLD"));
+
+38
+ const sentence = "JavaScript is amazing!";
+console.log(sentence.replace("amazing", "awesome"));
+
+39
+ const str = "Hello, World!";
+console.log(str.charCodeAt(4));
+
+40
+ const sentence = "JavaScript is powerful!";
+console.log(sentence.split(" ").length);
+
+41
+ const text = "Hello, World!";
+console.log(text.search("W"));
+
+42
+ const sentence = "JavaScript is dynamic!";
+ console.log(sentence.slice(-7));
+
+ 43
+const str = "Hello, World!";
+console.log(str.concat(" Goodbye!"));
+
+44
+ const sentence = "JavaScript is flexible!";
+console.log(sentence.charAt(sentence.length - 1));
+
+45
+const sentence = "JavaScript is interesting!";
+console.log(sentence.substring(10));
+
+46
+ const str = "Hello, World!";
+console.log(str.search("World"));
+
+47
+ const sentence = "JavaScript is versatile!";
+console.log(sentence.split(" ").reverse().join(" "));
+
+48
+ const sentence = "JavaScript is powerful!";
+console.log(sentence.substring(4, 10));
+
+49
+ const str = "Hello, World!";
+console.log(str.charCodeAt(0));
+
+50
+ const sentence = "JavaScript is amazing!";
+console.log(sentence.slice(4, 10));
+51
+ const text = "Hello, World!";
+console.log(text.indexOf("o", 5));
+
+52
+ const sentence = "JavaScript is fun!";
+console.log(sentence.startsWith("Java"));
+
+53
+ const str = "Hello, World!";
+console.log(str.endsWith("!"));
+
+54
+ const sentence = "JavaScript is amazing!";
+console.log(sentence.lastIndexOf("a", 12));
+
+55
+ const text = "Hello, World!";
+console.log(text.substring(7, 2));
+
+56
+ const sentence = "JavaScript is cool!";
+console.log(sentence.concat(" I love it!"));
+
+57
+ const str = "Hello, World!";
+console.log(str.toUpperCase().charAt(7));
+
+58
+ const str = "Hello, World!";
+console.log(str.toUpperCase().charAt(7));
+
+59
+const sentence = "JavaScript is amazing!";
+console.log(sentence.replace("amazing", "awesome").toUpperCase());
+
+60
+function is_string(input){
+    if(typeof input===’string’)
+            return true;
+   else return false;
+}
+console.log(is_string('w3resource'));      // true
+console.log(is_string([1, 2, 4, 0]));       // false
+
+61
+function is_Blank(input){
+    if(input=='')
+          return true;
+   else return false;
+ }
+ console.log(is_Blank(''));          // true
+ console.log(is_Blank('abc'));    // false
+ console.log(is_Blank(""));        // true
+ console.log(is_Blank(“ “)):       // false
+
+
+ 62
+ function string_to_array(input){
+    return input.split(‘ ‘);
+}
+console.log(string_to_array("Robin Singh"));   // ["Robin", "Singh"]
+console.log(string_to_array(“Robin Singh Chaddha”));
+// [ “Robin “Singh”, “Chaddha”]
+
+
+63
+function truncate_string(input,num){
+    return input.slice(0,num);
+}
+console.log(truncate_string("Robin Singh",4)); // "Robi"
+
+
+64
+function protect_email(email){
+    return email.replace(email.substring(5,email.indexOf('@')),'...');
+}
+console.log(protect_email("robin_sing@example.com")); 
+// robin…@example.com
+console.log(protect_email("pratham.pandya@example.com")); 
+// prath…@example.com
+
+
+66
+function capitalize(input){
+    return input.charAt(0).toUpperCase()+input.slice(1);
+}
+console.log(capitalize('js string exercises'));  // Js string exercises
+console.log(capitalize('hello it is me'));  // Hello it is me
+
+
+67
+function capitalize_words(input){
+    const words = input.split(' ');
+    for(var i=0; i< words.length;i++){
+    words[i] = words[i].charAt(0).toUpperCase()+words[i].slice(1);
+  }
+  return words.join(" ");
+}
+console.log(capitalize_words('js string exercises'));   // Js String Exercises
+
+
+68
+function insert(input,insertInput,position){
+    if(insertInput==undefined && position==undefined){
+                          return input;
+               }
+              else if(position==undefined){
+                          return insertInput.concat(input);
+               }
+               else return input.slice(0,position-1) +     
+               input.charAt(position-1).concat(insertInput)+input.slice(position);
+     }
+console.log(insert('We are doing some exercises.'));
+// We are doing some exercises.
+console.log(insert('We are doing some exercises.','JavaScript '));
+// JavaScript We are doing some exercises.
+console.log(insert('We are doing some exercises.','JavaScript ',18));
+// We are doing some JavaScript exercises.
+console.log(insert('We are doing some exercises.','JavaScript ',13));
+// We are doing JavaScript some exercises.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
