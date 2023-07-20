b54f21bd231af596e2c5ed6a6887f7a0561d47d8
bhakti hansalpara <bhakti.hansalpara@saeculum.com>
2023-07-20 13:11:46 +0530

    till 1:20

--- 19 july/object.js
+++ 19 july/object.js
@@ -315,20 +315,20 @@ Questions:
 // result.a.b.d = 3;
 // console.log(source);
 
-function myAssign(target, source) {
-    Object.entries(source).forEach(([key, value]) => {
-      target[key] = value;
-    });
-    return target;
-  }
+// function myAssign(target, source) {
+//     Object.entries(source).forEach(([key, value]) => {
+//       target[key] = value;
+//     });
+//     return target;
+//   }
   
-  // Example usage:
-  const obj1 = { a: 1, b: 2 };
-  const obj2 = { b: 3, c: 4 };
+//   // Example usage:
+//   const obj1 = { a: 1, b: 2 };
+//   const obj2 = { b: 3, c: 4 };
   
-  const result = myAssign(obj1, obj2);
+//   const result = myAssign(obj1, obj2);
   
-  console.log(result); // Output: { a: 1, b: 3, c: 4 }
+//   console.log(result); // Output: { a: 1, b: 3, c: 4 }
   
 
 // function myAssign(target, source) {
@@ -350,51 +350,51 @@ function myAssign(target, source) {
   
 
 // -----array question------//
-Write a visualization notes on given examples
-let obj1 = { a: 23, b: 45 };
-let arr1 = [{ ...obj1 }];
-let arr2 = arr1;
-arr2[0].c = 5;
-console.log(arr1)
+// Write a visualization notes on given examples
+// let obj1 = { a: 23, b: 45 };
+// let arr1 = [{ ...obj1 }];
+// let arr2 = arr1;
+// arr2[0].c = 5;
+// console.log(arr1)
 
-let obj1 = { a: 23, b: 45, c: { f: 990 } };
-let arr1 = [{ ...obj1 }];
-let arr2 = arr1;
-arr2[0].c = 5;
-obj1.c.f = 30;
-console.log(arr1); 
+// let obj1 = { a: 23, b: 45, c: { f: 990 } };
+// let arr1 = [{ ...obj1 }];
+// let arr2 = arr1;
+// arr2[0].c = 5;
+// obj1.c.f = 30;
+// console.log(arr1); 
 
-let obj1 = { a: 23, b: 45, c: { f: 990 } };
-let arr1 = [{ ...obj1 }];
-let arr2 = arr1;
-obj1.c.f = 30;
-console.log(arr1); 
+// let obj1 = { a: 23, b: 45, c: { f: 990 } };
+// let arr1 = [{ ...obj1 }];
+// let arr2 = arr1;
+// obj1.c.f = 30;
+// console.log(arr1); 
 
 
 
-let obj1 = { a: 23, b: 45 };
-let arr1 = Object.assign([], [{ ...obj1 }]);
-let arr2 = arr1;
-arr2[0].c = 5;
-console.log(arr1); 
+// let obj1 = { a: 23, b: 45 };
+// let arr1 = Object.assign([], [{ ...obj1 }]);
+// let arr2 = arr1;
+// arr2[0].c = 5;
+// console.log(arr1); 
 
-let obj1 = { a: 23, b: 45 };
-let arr1 = [{ ...obj1 }];
-let arr2 = Object.assign([], arr1);
-arr2[0].c = 5;
-console.log(arr1); 
+// let obj1 = { a: 23, b: 45 };
+// let arr1 = [{ ...obj1 }];
+// let arr2 = Object.assign([], arr1);
+// arr2[0].c = 5;
+// console.log(arr1); 
 
-let obj1 = { a: 23, b: 45 };
-let arr1 = [{ ...obj1 }];
-let arr2 = Object.assign([], [{...arr1[0]}]);
-arr2[0].c = 5;
-console.log(arr1); 
+// let obj1 = { a: 23, b: 45 };
+// let arr1 = [{ ...obj1 }];
+// let arr2 = Object.assign([], [{...arr1[0]}]);
+// arr2[0].c = 5;
+// console.log(arr1); 
 
-let obj1 = { a: 34, b: 56, c: { h: 77, j: 90 } };
-let obj2 = Object.assign({}, obj1);
-let obj3 = Object.assign({ ...obj2 }, { c: { k: 44 } });
-obj2.c.h = 67;
-console.log(obj1, obj2, obj3);
+// let obj1 = { a: 34, b: 56, c: { h: 77, j: 90 } };
+// let obj2 = Object.assign({}, obj1);
+// let obj3 = Object.assign({ ...obj2 }, { c: { k: 44 } });
+// obj2.c.h = 67;
+// console.log(obj1, obj2, obj3);
 
 
 // function f() {
@@ -419,11 +419,11 @@ console.log(obj1, obj2, obj3);
 // const obj3 = { ...obj1 };
 // obj1.c = 23;
 // console.log(obj1.c, obj2.c, obj3.c);
-const target={a:1}
-const source={b:2}
-const result=Object.assign(target,source)
-console.log(target);
-console.log(result===target);
+// const target={a:1}
+// const source={b:2}
+// const result=Object.assign(target,source)
+// console.log(target);
+// console.log(result===target);
 
 
 
